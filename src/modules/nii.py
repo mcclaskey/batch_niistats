f5b7b81214b47edf9337b42be6491c0b0bc45edc
CMcC <carolyn.mcclaskey@gmail.com>
2025-04-21 17:09:55 -0400

    volume is read from inputs when loading niftis
    This change alters the code that loads niftis so that the correct
    volume is loaded.
    
    Signed-off-by: CMcC <carolyn.mcclaskey@gmail.com>

--- src/modules/nii.py
+++ src/modules/nii.py
@@ -11,19 +11,22 @@
 
 import nibabel as nb
 import numpy as np
+from typing import Optional, Union, Dict, Set
 
-def load_nii(input_file: str) -> np.ndarray:
+def load_nii(input_file: str,
+             nii_volume: int) -> np.ndarray:
 
-    """ Call nibabel to load first volumn of .nii file
+    """ Call nibabel to load a volume of .nii file
 
-    We can't assume that array is 2D, so load to array and check. 
-    Returns a 3-D array.
+    Load a volume from a .nii file using nibabel.
+    Returns a 3D NumPy array.
 
     """
     img_proxy = nb.load(input_file)
     data_array = np.asarray(img_proxy.get_fdata())
+    
     if data_array.ndim == 4:
-        data_array = data_array[...,0] #get only first volume
+        data_array = data_array[...,nii_volume] #get only first volume
     
     return(data_array)
 
@@ -51,9 +54,11 @@ def sd_nii(data_array: np.ndarray,
     
     return value
 
-def single_nii_calc(nii_file: list[str],
-                            inputs: dict[bool,str],
-                            valid_files: list[str]) -> dict[str, float]:
+def single_nii_calc(nii_file: str,
+                    nii_volume: str,
+                    inputs: Dict[str, Union[bool, str]],
+                    valid_files: Set[str]
+                    ) -> Dict[str, Union[str, int, float]]:
     
     """Calculate statistics for a single .nii file, to be used with map
     
@@ -73,7 +78,7 @@ def single_nii_calc(nii_file: list[str],
 
     # Run calculation only if the file exists
     if nii_file in valid_files:
-        nii_array = load_nii(nii_file)
+        nii_array = load_nii(nii_file,nii_volume)
 
         if inputs["statistic"] == 'mean':
             output_val = mean_nii(nii_array, inputs["omit_zeros"])
