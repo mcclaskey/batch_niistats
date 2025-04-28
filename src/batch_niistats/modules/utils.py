3f6a482d2d62f072d7be18b48f168263c211d435
Carolyn McClaskey <carolyn.mcclaskey@gmail.com>
2025-04-28 13:26:02 -0400

    changing indentation from tabs to spaces
    Signed-off-by: Carolyn McClaskey <carolyn.mcclaskey@gmail.com>

--- src/batch_niistats/modules/utils.py
+++ src/batch_niistats/modules/utils.py
@@ -2,12 +2,12 @@
 # -*- coding : utf-8 -*-
 
 """
-	Functions for basic utilities, such as path lookups and reading 
-	input files.
-		
-	Part of batch_niistats package.
+    Functions for basic utilities, such as path lookups and reading 
+    input files.
+        
+    Part of batch_niistats package.
 
-	CMcC 4/21/2025 github: https://github.com/mcclaskey/batch_niistats. 
+    CMcC 4/21/2025 github: https://github.com/mcclaskey/batch_niistats. 
 
 """
 
@@ -24,122 +24,122 @@ def get_timestamp() -> str:
 
 def parse_inputs(input_arg: str) -> dict[str, bool | str] | None:
     """Parse user-provided input options
-	
+    
     Reads the user-provided option and defines the statistic
-	and whether to use all voxels or only non-zero voxels, 
-	then returns this as a dict.
+    and whether to use all voxels or only non-zero voxels, 
+    then returns this as a dict.
 
     Supported options are:
-	M: calculate mean of nonzero voxels
-	m: calculate mean of all voxels
-	S: calculate standard deviation of nonzero voxels
-	s: calculate standard deivation of all voxels
-	"""
-	
+    M: calculate mean of nonzero voxels
+    m: calculate mean of all voxels
+    S: calculate standard deviation of nonzero voxels
+    s: calculate standard deivation of all voxels
+    """
+    
     option_map = {
         "M": {"omit_zeros": True, "statistic": "mean"},
         "m": {"omit_zeros": False, "statistic": "mean"},
         "S": {"omit_zeros": True, "statistic": "sd"},
         "s": {"omit_zeros": False, "statistic": "sd"},
     }
-	
+    
     return option_map.get(input_arg, {})
 
 
 def askfordatalist() -> str:
-	"""Prompt user for input CSV file and return full file path as string."""
-	root = tk.Tk()
-	root.withdraw()
-	return filedialog.askopenfilename()
+    """Prompt user for input CSV file and return full file path as string."""
+    root = tk.Tk()
+    root.withdraw()
+    return filedialog.askopenfilename()
 
 
 def comma_split(input_spm_path: str) -> dict[str, int | None]:
-	"""Split SPM-style path at comma, return file and 0-based vol as dict"""
-	parts = input_spm_path.split(',')
-	if len(parts) > 1 and parts[1].isdigit():
-		volume_index  = int(parts[1]) - 1
-	else:
-		volume_index  = None
-		
+    """Split SPM-style path at comma, return file and 0-based vol as dict"""
+    parts = input_spm_path.split(',')
+    if len(parts) > 1 and parts[1].isdigit():
+        volume_index  = int(parts[1]) - 1
+    else:
+        volume_index  = None
+        
 
-	return {'file': parts[0],'volume_spm_0basedindex': volume_index }
+    return {'file': parts[0],'volume_spm_0basedindex': volume_index }
 
 def parse_spmsyntax(datalist: pd.DataFrame) -> pd.DataFrame:
-	"""Handle SPM-style volume syntax in 'input_file' column
+    """Handle SPM-style volume syntax in 'input_file' column
 
     Takes .csv datalist and reads the "input_file" column according to SPM
-	syntax for specifying volumes. Returns a dataframe where the input_file
-	column is converted to a pure filepath and a new 0-based index column
-	containing the SPM volume is added.
+    syntax for specifying volumes. Returns a dataframe where the input_file
+    column is converted to a pure filepath and a new 0-based index column
+    containing the SPM volume is added.
     """
 
-	list_of_spmsplit = list(map(comma_split,datalist['input_file']))
-	df_of_spmsplits = pd.DataFrame(list_of_spmsplit)
+    list_of_spmsplit = list(map(comma_split,datalist['input_file']))
+    df_of_spmsplits = pd.DataFrame(list_of_spmsplit)
 
-	return pd.concat([datalist, df_of_spmsplits], axis=1)
+    return pd.concat([datalist, df_of_spmsplits], axis=1)
 
 def prioritize_volume(datalist):
-	"""Determine which volume to read for each file given input info
-	
-	Reads datalist with potentially multiple volumn columns and resolves 
-	conflicting or missing values. Determines which volume to read according
-	to rules and returns a dataframe with only two columns: 'input_file' which
-	has only a pure file path to .nii, and 'volume_0basedindex' column.
+    """Determine which volume to read for each file given input info
+    
+    Reads datalist with potentially multiple volumn columns and resolves 
+    conflicting or missing values. Determines which volume to read according
+    to rules and returns a dataframe with only two columns: 'input_file' which
+    has only a pure file path to .nii, and 'volume_0basedindex' column.
 
     Preference order: explicit volume col > SPM syntax > default to first vol.
     """
-	
-	# temp var
-	datalist['volume'] = None	
-	
-	# uses matching volume
-	matches = datalist['volume_spm_0basedindex'] == datalist['volume_0basedindex']
-	datalist.loc[matches,'volume'] = datalist.loc[matches,'volume_0basedindex']
+    
+    # temp var
+    datalist['volume'] = None	
+    
+    # uses matching volume
+    matches = datalist['volume_spm_0basedindex'] == datalist['volume_0basedindex']
+    datalist.loc[matches,'volume'] = datalist.loc[matches,'volume_0basedindex']
 
-	# if conflicts, preferentially read from user created column 'volume_0basedindex'
-	user_vol = ~np.isnan(datalist.loc[~matches, 'volume_0basedindex'])
-	datalist.loc[~matches & user_vol, 'volume'] = datalist.loc[~matches & user_vol, 'volume_0basedindex']
+    # if conflicts, preferentially read from user created column 'volume_0basedindex'
+    user_vol = ~np.isnan(datalist.loc[~matches, 'volume_0basedindex'])
+    datalist.loc[~matches & user_vol, 'volume'] = datalist.loc[~matches & user_vol, 'volume_0basedindex']
 
-	# if missingness, read from spm
-	missingrows = datalist['volume'].isna()
-	spm_vol = ~np.isnan(datalist.loc[missingrows,'volume_spm_0basedindex'])
-	datalist.loc[missingrows & spm_vol ,'volume'] = datalist.loc[missingrows & spm_vol ,'volume_spm_0basedindex']
+    # if missingness, read from spm
+    missingrows = datalist['volume'].isna()
+    spm_vol = ~np.isnan(datalist.loc[missingrows,'volume_spm_0basedindex'])
+    datalist.loc[missingrows & spm_vol ,'volume'] = datalist.loc[missingrows & spm_vol ,'volume_spm_0basedindex']
 
-	# if missing, assume first volume
-	datalist.loc[datalist['volume'].isna(), 'volume'] = 0  # default to first volume
-	datalist['volume_0basedindex'] = datalist['volume'].astype(int)
-	return datalist.drop(columns=['volume_spm_0basedindex', 'volume'], errors='ignore')
+    # if missing, assume first volume
+    datalist.loc[datalist['volume'].isna(), 'volume'] = 0  # default to first volume
+    datalist['volume_0basedindex'] = datalist['volume'].astype(int)
+    return datalist.drop(columns=['volume_spm_0basedindex', 'volume'], errors='ignore')
 
 
 def load_datalist(datalist_filepath: str) -> pd.DataFrame:
-	"""Load user-specified input .csv file and return formatting dataframe
-	
-	Loads a CSV file containing paths to .nii files and optional volume indices.
+    """Load user-specified input .csv file and return formatting dataframe
+    
+    Loads a CSV file containing paths to .nii files and optional volume indices.
 
     Handles SPM-style syntax and fills in missing volume data. Resolves conflicting
-	input information and defaults to first volume where necessary. 
+    input information and defaults to first volume where necessary. 
 
-	Returns a dataframe with 'input_file' as pure absolute paths to .nii files 
-	and 'volume_0basedindex' column with volume indices. Other columns in the
-	datalist, if existing, are left unmodified.
+    Returns a dataframe with 'input_file' as pure absolute paths to .nii files 
+    and 'volume_0basedindex' column with volume indices. Other columns in the
+    datalist, if existing, are left unmodified.
     """
-	datalist = pd.read_csv(datalist_filepath)
+    datalist = pd.read_csv(datalist_filepath)
 
-	# now check for SPM volume syntax
-	if datalist['input_file'].astype(str).str.contains(',').any():
-		datalist = parse_spmsyntax(datalist)
-	else:
-		datalist['file'] = datalist['input_file']
-		datalist['volume_spm_0basedindex'] = np.nan
+    # now check for SPM volume syntax
+    if datalist['input_file'].astype(str).str.contains(',').any():
+        datalist = parse_spmsyntax(datalist)
+    else:
+        datalist['file'] = datalist['input_file']
+        datalist['volume_spm_0basedindex'] = np.nan
 
-	if 'volume_0basedindex' not in datalist.columns:
-		datalist['volume_0basedindex'] = np.nan
+    if 'volume_0basedindex' not in datalist.columns:
+        datalist['volume_0basedindex'] = np.nan
 
-	return prioritize_volume(datalist)
+    return prioritize_volume(datalist)
 
 def create_output_df(datalist: pd.DataFrame,
-					 list_of_data:list) -> pd.DataFrame:
-	
+                     list_of_data:list) -> pd.DataFrame:
+    
     """Merges input and output df and returns df with original index order"""
     calculated_df = pd.DataFrame(list_of_data)
     calculated_df['input_file'] = calculated_df['input_file'].str.strip()
@@ -151,7 +151,7 @@ def create_output_df(datalist: pd.DataFrame,
 
     combined_df = input_df.merge(calculated_df, on = ['input_file','index'], how='outer', sort = False)
     combined_df = combined_df.sort_values(by='index').drop(columns=["index"], axis=1).reset_index(drop=True)
-	
+    
     return combined_df
 
 def write_output_df_path(datalist_filepath: str,
@@ -162,13 +162,13 @@ def write_output_df_path(datalist_filepath: str,
     Output file will be in the same directory as input .csv.
     File name includes the timestamp and statistic.
     """
-	
-	timestamp_dt = datetime.datetime.strptime(timestamp,"%Y.%m.%d %H:%M:%S")
-	timestamp_file = timestamp_dt.strftime("%Y%m%d_%H%M%S")
-	statistic_clean = statistic.replace('-', '')
+    
+    timestamp_dt = datetime.datetime.strptime(timestamp,"%Y.%m.%d %H:%M:%S")
+    timestamp_file = timestamp_dt.strftime("%Y%m%d_%H%M%S")
+
+    output_dir = os.path.dirname(datalist_filepath)
+    base_name = os.path.basename(datalist_filepath).replace('.csv', f'_calc_{statistic}.csv')
+    output_path = os.path.join(output_dir, f"{timestamp_file}_{base_name}")
 
     return(output_path)
-	output_dir = os.path.dirname(datalist_filepath)
-	base_name = os.path.basename(datalist_filepath).replace('.csv', f'_calc_{statistic_clean}.csv')
-	output_path = os.path.join(output_dir, f"{timestamp_file}_{base_name}")
 
